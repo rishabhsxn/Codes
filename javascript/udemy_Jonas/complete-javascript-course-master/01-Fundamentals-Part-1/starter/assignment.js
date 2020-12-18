15de978a9c95b82a48dc94b9eb6652f673e05b25
Vold <rishabhsaxena1998@gmail.com>
2020-12-19 01:59:13 +0530

    Udemy - Javascript course (Jonas): Initial commit

---
+++ javascript/udemy_Jonas/complete-javascript-course-master/01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,7 @@
+let country = "India";
+let continent = "Asia";
+let population = 1352.6; // million
+
+console.log("Country: " + country);
+console.log("Continent: " + continent);
+console.log("Population: " + population + " million");
---
+++ javascript/udemy_Jonas/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,68 @@
+// let js = "amazing";
+// // if(js=="amazing") alert('JavaScript is amazing');
+
+// 40 + 23 + 21 - 10;
+
+// console.log(40+23+21-10);
+
+
+// Challenge #1
+
+// const markMass = 78;      // kg
+// const markHeight = 1.69;      // m
+// const markBMI = markMass / markHeight**2;
+
+// const johnMass = 92;
+// const johnHeight = 1.95;
+// const johnBMI = johnMass / johnHeight**2;
+
+// const markHigherBMI = markBMI > johnBMI;
+
+// // if(markBMI > johnBMI)
+// //     markHigherBMI = true;
+// // else
+// //     markHigherBMI = false;
+
+// console.log(markHigherBMI);
+
+
+// // template literals
+// const firstName = "Rishabh";
+// const age = 22;
+// const job = "Software Engineer";
+
+// const rishabh = "I'm " + firstName + ", a " + age + " years old " + job;
+// console.log(rishabh);
+
+// // This is alternate way of making above string, introduced in ES6
+// const rishabhNew = `I'm ${firstName}, a ${age} years old ${job}`;
+// console.log(rishabhNew);
+
+
+// console.log("This is\n\
+// a multiline\n\
+// string")
+
+// console.log(`Using backticks,
+// we can write
+// this directly`)
+
+
+// // challenge #2
+
+// const markMass = 78;      // kg
+// const markHeight = 1.69;      // m
+// const markBMI = markMass / markHeight**2;
+
+// const johnMass = 92;
+// const johnHeight = 1.95;
+// const johnBMI = johnMass / johnHeight**2;
+
+// if(johnBMI > markBMI)
+//     console.log(`John's BMI (${johnBMI}) is greater than Mark's BMI (${markBMI})`);
+// else if(markBMI > johnBMI)
+//     console.log(`Mark's BMI (${markBMI}) is greater than John's BMI (${johnBMI})`);
+// else
+//     console.log(`John's BMI (${johnBMI}) is equal to Mark's BMI (${markBMI})`);
+    
+ 
