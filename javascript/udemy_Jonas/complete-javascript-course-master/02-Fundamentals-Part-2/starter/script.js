025f59422331178ab4863bb774671c23247cc690
rishabhsxn <rishabhsaxena1998@gmail.com>
2020-12-25 17:55:14 +0530

    Coding Challenge #4

--- javascript/udemy_Jonas/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ javascript/udemy_Jonas/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -117,48 +117,100 @@
 //   console.log(jonas.age);
 
 
-// --------------------------Coding Challenge #3----------------------------
+// // --------------------------Coding Challenge #3----------------------------
 
+// /*
+// Let's go back to Mark and John comparing their BMIs! This time, let's use objects to implement the calculations! 
+// Remember: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter)
+
+// 1. For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith)
+// 2. Create a 'calcBMI' method on each object to calculate the BMI (the same method on both objects). Store the BMI value to 
+// a property, and also return it from the method.
+// 3. Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John Smith's 
+// BMI (28.3) is higher than Mark Miller's (23.9)!"
+
+// TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+
+// GOOD LUCK 😀
+// */
+
+// const mark = {
+//   firstName: "Mark Miller",
+//   mass: 78,
+//   height: 1.69,
+
+//   calcBMI: function(){
+//     this.BMI = this.mass / this.height ** 2;
+//     return this.BMI;
+//   },
+// };
+
+// const john = {
+//   firstName: "John Smith",
+//   mass: 92,
+//   height: 1.95,
+
+//   calcBMI: function(){
+//     this.BMI = this.mass / this.height ** 2;
+//     return this.BMI;
+//   },
+// };
+
+// let personWithLessBMI, personWithMoreBMI;
+
+// personWithMoreBMI = mark.calcBMI() > john.calcBMI() ? mark : john;
+// personWithLessBMI = personWithMoreBMI == mark ? john : mark;
+
+// console.log(`${personWithMoreBMI.firstName}'s BMI (${personWithMoreBMI.BMI}) is higher than ${personWithLessBMI.firstName}'s BMI (${personWithLessBMI.BMI})`);
+
+
+// ------------------------------Coding Challenge #4-------------------------------
 /*
-Let's go back to Mark and John comparing their BMIs! This time, let's use objects to implement the calculations! 
-Remember: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter)
+Let's improve Steven's tip calculator even more, this time using loops!
 
-1. For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith)
-2. Create a 'calcBMI' method on each object to calculate the BMI (the same method on both objects). Store the BMI value to 
-a property, and also return it from the method.
-3. Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John Smith's 
-BMI (28.3) is higher than Mark Miller's (23.9)!"
+1. Create an array 'bills' containing all 10 test bill values
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate tips and total values (bill + tip) for every bill 
+value in the bills array. Use a for loop to perform the 10 calculations!
 
-TEST DATA: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+TEST DATA: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+
+HINT: Call calcTip in the loop and use the push method to add values to the tips and totals arrays 😉
+
+4. BONUS: Write a function 'calcAverage' which takes an array called 'arr' as an argument. This function calculates the average
+of all numbers in the given array. This is a DIFFICULT challenge (we haven't done this before)! Here is how to solve it:
+  4.1. First, you will need to add up all values in the array. To do the addition, start by creating a variable 'sum' that starts 
+  at 0. Then loop over the array using a for loop. In each iteration, add the current value to the 'sum' variable. This way, by 
+  the end of the loop, you have all values added together
+  4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements)
+  4.3. Call the function with the 'totals' array
 
 GOOD LUCK 😀
 */
 
-const mark = {
-  firstName: "Mark Miller",
-  mass: 78,
-  height: 1.69,
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52]
+const tips = []
+const totals = []
 
-  calcBMI: function(){
-    this.BMI = this.mass / this.height ** 2;
-    return this.BMI;
-  },
-};
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
 
-const john = {
-  firstName: "John Smith",
-  mass: 92,
-  height: 1.95,
+for(let i=0; i < bills.length; i++){
+  tips.push(calcTip(bills[i]));
+  totals.push(bills[i] + tips[i]);
+}
 
-  calcBMI: function(){
-    this.BMI = this.mass / this.height ** 2;
-    return this.BMI;
-  },
-};
+console.log(bills); 
+console.log(tips);
+console.log(totals);
 
-let personWithLessBMI, personWithMoreBMI;
+const calcAverage = function(arr){
+  let sum = 0;
+  for(let i=0; i<arr.length; i++)
+    sum += arr[i];
+  
+  return sum/arr.length;
+}
 
-personWithMoreBMI = mark.calcBMI() > john.calcBMI() ? mark : john;
-personWithLessBMI = personWithMoreBMI == mark ? john : mark;
-
-console.log(`${personWithMoreBMI.firstName}'s BMI (${personWithMoreBMI.BMI}) is higher than ${personWithLessBMI.firstName}'s BMI (${personWithLessBMI.BMI})`);
+console.log(calcAverage(totals));
