1ec0e5b8a29ce0968e3e2bf5adbbe1f94cfe9cad
rishabhsxn <rishabhsaxena1998@gmail.com>
2020-12-26 23:02:37 +0530

    Selecting & modifying Elements through JavaScript

--- javascript/udemy_Jonas/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ javascript/udemy_Jonas/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,1 +1,14 @@
 'use strict';
+
+// selecting elements
+console.log(document.querySelector(".message").textContent);
+
+document.querySelector(".message").textContent = "Correct Answer!"
+
+document.querySelector(".score").textContent = 30;
+document.querySelector(".highscore").textContent = 50;
+
+document.querySelector(".number").textContent = 10
+
+// selecting input element
+document.querySelector(".guess").value = 10;
