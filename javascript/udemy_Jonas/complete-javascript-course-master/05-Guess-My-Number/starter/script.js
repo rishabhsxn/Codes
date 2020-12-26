a5f06edb188241b05c426adff01329a06a654524
rishabhsxn <rishabhsaxena1998@gmail.com>
2020-12-26 23:16:08 +0530

    Add Event Handler to the button

--- javascript/udemy_Jonas/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ javascript/udemy_Jonas/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,14 +1,28 @@
 'use strict';
 
 // selecting elements
-console.log(document.querySelector(".message").textContent);
+// console.log(document.querySelector(".message").textContent);
 
-document.querySelector(".message").textContent = "Correct Answer!"
+// document.querySelector(".message").textContent = "Correct Answer!"
 
-document.querySelector(".score").textContent = 30;
-document.querySelector(".highscore").textContent = 50;
+// document.querySelector(".score").textContent = 30;
+// document.querySelector(".highscore").textContent = 50;
 
-document.querySelector(".number").textContent = 10
+// document.querySelector(".number").textContent = 10
 
-// selecting input element
-document.querySelector(".guess").value = 10;
+// // selecting input element
+// document.querySelector(".guess").value = 10;
+
+// ------------- Adding Event handler for the button ---------------
+
+/* The check button have two class name "btn check", since btn was also used for another button,
+we used the check class to select the check button 
+We are not calling the handler function but just passing it, the JavaScript Engine will execute it automatically
+whenever the event happens */
+
+document.querySelector(".check").addEventListener("click", function(){
+    // the input will be string, so convert to number
+    const guess = Number(document.querySelector(".guess").value);
+    console.log(guess, typeof guess);
+
+})
