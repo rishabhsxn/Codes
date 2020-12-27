55dbcb20aaf248a6326dca481ac042884db22233
rishabhsxn <rishabhsaxena1998@gmail.com>
2020-12-27 14:15:41 +0530

    Generate Secret number & Handle input cases

--- javascript/udemy_Jonas/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ javascript/udemy_Jonas/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -13,6 +13,11 @@
 // // selecting input element
 // document.querySelector(".guess").value = 10;
 
+
+// generate Secret number (random number)
+const secretNumber = Math.trunc(Math.random()*20) + 1;
+document.querySelector(".number").textContent = secretNumber;
+
 // ------------- Adding Event handler for the button ---------------
 
 /* The check button have two class name "btn check", since btn was also used for another button,
@@ -24,5 +29,19 @@ document.querySelector(".check").addEventListener("click", function(){
     // the input will be string, so convert to number
     const guess = Number(document.querySelector(".guess").value);
     console.log(guess, typeof guess);
+
+    // if there is no number in input, guess will be zero which is a falsy value
+    if(!guess){
+        document.querySelector(".message").textContent = "⛔️ No Number!";
+    }
+    else if(guess === secretNumber){
+        document.querySelector(".message").textContent = "🎉 Correct Number!";
+    }
+    else if(guess > secretNumber){
+        document.querySelector(".message").textContent = "📈 Too high!";
+    }
+    else{
+        document.querySelector(".message").textContent = "📉 Too low!";
+    }
 
 })
