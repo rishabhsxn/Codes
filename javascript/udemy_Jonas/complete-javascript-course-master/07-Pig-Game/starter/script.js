4bdbecf8f84cd565bb947e1fce536748b8b8ce28
rishabhsxn <rishabhsaxena1998@gmail.com>
2020-12-30 22:54:19 +0530

    Reset the game to initial stage (lookwise)

--- javascript/udemy_Jonas/complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ javascript/udemy_Jonas/complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -16,3 +16,10 @@ const player1_currentScore = document.querySelector("#current--0");
 
 const player2_score = document.querySelector("#score--1");
 const player2_currentScore = document.querySelector("#current--1");
+
+
+
+// set the game to initial stage i.e. score = 0 and no dice visible
+player1_score.textContent = 0;
+player2_score.textContent = 0;
+diceImage.style.display = "none";
