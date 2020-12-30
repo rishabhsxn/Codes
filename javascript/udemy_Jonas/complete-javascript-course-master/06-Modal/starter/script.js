80e18168336a21cd09afe77e3516cb6348f6b382
rishabhsxn <rishabhsaxena1998@gmail.com>
2020-12-30 13:02:41 +0530

    Select the elements that we will manipulate

--- javascript/udemy_Jonas/complete-javascript-course-master/06-Modal/starter/script.js
+++ javascript/udemy_Jonas/complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+// selecting the elements
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+
+// this will have multiple button elements
+const btn_showModal = document.querySelectorAll(".show-modal");
+const btn_closeModal = document.querySelector(".close-modal");
+
