a56cf1d6c5bc7f435c227240fe979f2849a9f168
rishabhsxn <rishabhsaxena1998@gmail.com>
2020-12-30 22:42:19 +0530

    Select Elements that are to be manipulated

--- javascript/udemy_Jonas/complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ javascript/udemy_Jonas/complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+
+// select the elements to manipulate
+
+const diceImage = document.querySelector(".dice");
+
+// buttons
+const btn_rollDice = document.querySelector(".btn--roll");
+const btn_hold = document.querySelector(".btn--hold");
+const btn_newGame = document.querySelector(".btn--new");
+
+// scores
+const player1_score = document.querySelector("#score--0");
+const player1_currentScore = document.querySelector("#current--0");
+
+const player2_score = document.querySelector("#score--1");
+const player2_currentScore = document.querySelector("#current--1");
