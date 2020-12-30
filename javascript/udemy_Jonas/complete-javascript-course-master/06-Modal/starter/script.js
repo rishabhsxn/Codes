96b0d3c585685aa01b0facc8607e50ecc91077e9
rishabhsxn <rishabhsaxena1998@gmail.com>
2020-12-30 13:14:02 +0530

    Add Event Listener to ShowModal and CloseModal

--- javascript/udemy_Jonas/complete-javascript-course-master/06-Modal/starter/script.js
+++ javascript/udemy_Jonas/complete-javascript-course-master/06-Modal/starter/script.js
@@ -8,3 +8,17 @@ const overlay = document.querySelector(".overlay");
 const btn_showModal = document.querySelectorAll(".show-modal");
 const btn_closeModal = document.querySelector(".close-modal");
 
+// functions
+const showModal = function(){
+    console.log("Show Modal button pressed")
+}
+
+const closeModal = function(){
+    console.log("Close Modal button pressed")
+}
+
+// add eventlistner to the buttons
+for(let i=0; i<btn_showModal.length; i++)
+    btn_showModal[i].addEventListener("click", showModal)
+
+btn_closeModal.addEventListener("click", closeModal)
