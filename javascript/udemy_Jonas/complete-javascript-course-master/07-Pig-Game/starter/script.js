9b7a49d2adb53cf967d4701fc384e8817a59c784
rishabhsxn <rishabhsaxena1998@gmail.com>
2021-01-02 13:37:00 +0530

    Implement - End game when win condition (Roll Dice & Hold should not work)

--- javascript/udemy_Jonas/complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ javascript/udemy_Jonas/complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -3,6 +3,7 @@
 // global variables
 let activePlayer, currentScore;
 const score = [0, 0];
+let isPlaying = true;
 
 // select the elements to manipulate
 const diceImage = document.querySelector(".dice");
@@ -25,52 +26,56 @@ currentScore = 0;
 
 // functions
 const rollDice = function(){
-    // generate a random number 1 to 6
-    const diceNumber = Math.trunc(Math.random()*6) + 1;
+    if(isPlaying){
+        // generate a random number 1 to 6
+        const diceNumber = Math.trunc(Math.random()*6) + 1;
 
-    // set diceImage of that number and unhide
-    diceImage.classList.remove("hidden")
-    diceImage.src = `dice-${diceNumber}.png`
+        // set diceImage of that number and unhide
+        diceImage.classList.remove("hidden")
+        diceImage.src = `dice-${diceNumber}.png`
 
-    // if the number is not equal to 1, add it to currentScore(variable and display) of activePlayer
-    if(diceNumber !== 1){
-        currentScore += diceNumber;
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        // if the number is not equal to 1, add it to currentScore(variable and display) of activePlayer
+        if(diceNumber !== 1){
+            currentScore += diceNumber;
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        }
+        // if it is 1, set currentScore(variable and display) of activePlayer=0, toggle activePlayer(variable and visual)
+        else{
+            currentScore = 0;
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+
+            activePlayer = activePlayer === 0 ? 1 : 0;
+            player1_Element.classList.toggle("player--active");
+            player2_Element.classList.toggle("player--active");
+        }
     }
-    // if it is 1, set currentScore(variable and display) of activePlayer=0, toggle activePlayer(variable and visual)
-    else{
-        currentScore = 0;
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        player1_Element.classList.toggle("player--active");
-        player2_Element.classList.toggle("player--active");
-    }
-    
 }
 
 const hold = function(){
-    // add the currentScore to totalScore of activePlayer (variable and visually)
-    score[activePlayer] += currentScore;
-    document.getElementById(`score--${activePlayer}`).textContent = score[activePlayer];
+    if(isPlaying){
+        // add the currentScore to totalScore of activePlayer (variable and visually)
+        score[activePlayer] += currentScore;
+        document.getElementById(`score--${activePlayer}`).textContent = score[activePlayer];
 
-    // make currentScore=0 (variable and visually)
-    currentScore = 0;
-    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        // make currentScore=0 (variable and visually)
+        currentScore = 0;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
 
-    // check if the total score of active player is >= 100, he wins 
-    if(score[activePlayer] >= 20){
-        document.querySelector(`.player--${activePlayer}`).classList.add("player--winner");
-        player1_Element.classList.remove("player--active");
-        player2_Element.classList.remove("player--active");
+        // check if the total score of active player is >= 100, he wins and end game(roll dice and hold should not work)
+        if(score[activePlayer] >= 20){
+            document.querySelector(`.player--${activePlayer}`).classList.add("player--winner");
+            player1_Element.classList.remove("player--active");
+            player2_Element.classList.remove("player--active");
+
+            isPlaying = false;
+        }
+        // toggle player if no win yet
+        else{
+            activePlayer = activePlayer === 0 ? 1 : 0;
+            player1_Element.classList.toggle("player--active");
+            player2_Element.classList.toggle("player--active");
+        }
     }
-    // toggle player if no win yet
-    else{
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        player1_Element.classList.toggle("player--active");
-        player2_Element.classList.toggle("player--active");
-    }
-
 }
 
 const newGame = function(){
