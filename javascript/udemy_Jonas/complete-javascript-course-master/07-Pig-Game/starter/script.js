709cf19fb9819ff075c43e43e720ee1af53da0f6
rishabhsxn <rishabhsaxena1998@gmail.com>
2021-01-02 12:47:29 +0530

    Variables Refactoring

--- javascript/udemy_Jonas/complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ javascript/udemy_Jonas/complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -4,28 +4,19 @@
 let activePlayer, currentScore;
 
 // select the elements to manipulate
-
 const diceImage = document.querySelector(".dice");
 const player1_Element = document.querySelector(".player--0");
 const player2_Element = document.querySelector(".player--1");
 
-// buttons
 const btn_rollDice = document.querySelector(".btn--roll");
 const btn_hold = document.querySelector(".btn--hold");
 const btn_newGame = document.querySelector(".btn--new");
 
-// scores
-const player1_score = document.getElementById("score--0");
-const player1_currentScore = document.getElementById("current--0");
-
-const player2_score = document.getElementById("score--1");
-const player2_currentScore = document.getElementById("current--1");
-
 
 
 // set the game to initial stage i.e. score = 0 and no dice visible
-player1_score.textContent = 0;
-player2_score.textContent = 0;
+document.getElementById("score--0").textContent = 0;
+document.getElementById("score--1").textContent = 0;
 diceImage.classList.add("hidden");
 activePlayer = 0;
 currentScore = 0;
