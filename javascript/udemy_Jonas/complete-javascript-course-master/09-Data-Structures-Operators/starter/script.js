3686b1b997b7f14c8c8e553e4ca41652560b4a1a
rishabhsxn <rishabhsaxena1998@gmail.com>
2021-01-06 13:42:23 +0530

    Destructuring Arrays

--- javascript/udemy_Jonas/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ javascript/udemy_Jonas/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -21,4 +21,35 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function(mainIndex, starterIndex){
+    return [this.mainMenu[mainIndex], this.starterMenu[starterIndex]];    // receive this value and immediately destructure
+  }
 };
+
+
+// ----------------------------- DESTRUCTURING OF ARRAYS ------------------------------
+
+const arr = [1, 2, 3];
+
+/* To skip a value, leave space for it
+We can also add default values -> handy in cases when the array doesn't have mentioned index element*/
+let [a, , b, c=0] = arr;
+console.log(a, b, c);
+
+
+//---------- Tricks using Destructuring ---------------
+
+// 1. swapping of variables
+console.log(a, b);
+[a, b] = [b, a];
+console.log(a, b);
+
+// 2. return multiple values from a function
+const [mainDish, starterDish] = restaurant.order(2, 1);
+console.log(mainDish, starterDish);
+
+// 3. Extract values from nested Arrays
+const nestedArr = [2, 4, [5, 7]];
+let [x, , [y, z]] = nestedArr;
+console.log(x, y, z);
