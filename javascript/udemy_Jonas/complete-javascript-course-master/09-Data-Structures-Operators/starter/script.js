b16f7121cb85ed417f56fd795698cf4a85ca233f
rishabhsxn <rishabhsaxena1998@gmail.com>
2021-01-07 23:35:46 +0530

    Spread Operator (...)

--- javascript/udemy_Jonas/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ javascript/udemy_Jonas/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -26,11 +26,15 @@ const restaurant = {
   //   return [this.mainMenu[mainIndex], this.starterMenu[starterIndex]];    // receive this value and immediately destructure
   // },
 
-  /* When we need to pass a lot of parameters to a function, remembering order of parameters may be difficult.
-  So, we can pass the parameters as properties of an object and destructure it right there in the function
-  definition. We can even set default values */
-  orderDelivery: function({starterIndex, mainIndex, time="22:00", address}){
-    console.log(`Order Received: ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be deliverd to ${address} at ${time}`);
+  // /* When we need to pass a lot of parameters to a function, remembering order of parameters may be difficult.
+  // So, we can pass the parameters as properties of an object and destructure it right there in the function
+  // definition. We can even set default values */
+  // orderDelivery: function({starterIndex, mainIndex, time="22:00", address}){
+  //   console.log(`Order Received: ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be deliverd to ${address} at ${time}`);
+  // },
+
+  orderPasta: function(ingredient1, ingredient2, ingredient3){
+    console.log(`Your pasta is ready with ${ingredient1}, ${ingredient2} and ${ingredient3}`);
   }
 };
 
@@ -62,43 +66,76 @@ const restaurant = {
 // console.log(x, y, z);
 
 
-// ----------------------------- DESTRUCTURING OF OBJECTS ------------------------------
+// // ----------------------------- DESTRUCTURING OF OBJECTS ------------------------------
 
-/* To extract from an object, write the exact property names. Order doesn't matter.
+// /* To extract from an object, write the exact property names. Order doesn't matter.
 
-To use other variable name use below syntax */
-const {name: restaurantName, openingHours, categories} = restaurant;
-console.log(restaurantName, openingHours, categories);
-
-
-// for default values (helpful in APIs)
-const {menu: menuItems = []} = restaurant;
-console.log(menuItems);
+// To use other variable name use below syntax */
+// const {name: restaurantName, openingHours, categories} = restaurant;
+// console.log(restaurantName, openingHours, categories);
 
 
-// IMPORTANT: mutating variables in case when variable name is same as property name
-let a = 111;
-let b = 999;
-const obj = {
-  a: 23,
-  b: 7,
-  c: 10
-};
-
-// let {a, b} = obj;     // can't write this because it gives an error, variables are already declared
-// {a, b} = obj;     // can't write because JS excepts a code block when {} is used
-// To over come this, use paranthesis to wrap
-({a, b} = obj);
-console.log(a, b);
+// // for default values (helpful in APIs)
+// const {menu: menuItems = []} = restaurant;
+// console.log(menuItems);
 
 
-//  Nested Objects + renaming variable
-const {fri: {open: o, close: c}} = openingHours;
-console.log(o, c);
+// // IMPORTANT: mutating variables in case when variable name is same as property name
+// let a = 111;
+// let b = 999;
+// const obj = {
+//   a: 23,
+//   b: 7,
+//   c: 10
+// };
 
-// passing parameters as properties of an object
-restaurant.orderDelivery({
-  address: "White house",
-  mainIndex: 0,
-  starterIndex: 2
-});
+// // let {a, b} = obj;     // can't write this because it gives an error, variables are already declared
+// // {a, b} = obj;     // can't write because JS excepts a code block when {} is used
+// // To over come this, use paranthesis to wrap
+// ({a, b} = obj);
+// console.log(a, b);
+
+
+// //  Nested Objects + renaming variable
+// const {fri: {open: o, close: c}} = openingHours;
+// console.log(o, c);
+
+// // passing parameters as properties of an object
+// restaurant.orderDelivery({
+//   address: "White house",
+//   mainIndex: 0,
+//   starterIndex: 2
+// });
+
+
+
+// ---------------------------- SPREAD OPERATOR -----------------------------
+const arr = [7, 8, 9];
+/* it unpacks the array into individual comma separated values.
+It doesn't affect the original array */
+const newArr = [...arr, 10, 11];
+
+console.log(newArr);
+console.log(...newArr);
+
+
+/* copy array -> This creates a new array, but if there was an array inside mainMenu, the inside array
+would not be re-created.  i.e. SHALLOW COPY */
+const mainMenuCopy = [...restaurant.mainMenu];
+console.log(mainMenuCopy);
+
+
+// merging two arrays
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(menu);
+
+
+// function call for multiple parameters
+const ingredients = [prompt("For you pizza, enter Ingredient 1: "), prompt("Ingredient 2: "), prompt("Ingredient 3: ")];
+restaurant.orderPasta(...ingredients);
+
+// Since 2018, the spread operator also work on objects even though they are not iterables
+const restaurantCopy = {...restaurant, founder: "Rishabh"};   // SHALLOW COPY
+restaurantCopy.mainMenu.push("Roti");
+console.log(restaurant.mainMenu);
+console.log(restaurantCopy.mainMenu);
