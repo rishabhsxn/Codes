08f4cdd4322ae23f0edd35d028b23ac1ff0e01f7
rishabhsxn <rishabhsaxena1998@gmail.com>
2021-01-18 20:57:17 +0530

    Coding Challenge #1

--- javascript/udemy_Jonas/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ javascript/udemy_Jonas/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -231,31 +231,131 @@ const restaurant = {
 
 
 
-// --------------------------- ENHANCED OBJECT LITERALS --------------------------
+// // --------------------------- ENHANCED OBJECT LITERALS --------------------------
 
-const menu = {
-  starter: "Chicken Wings",
-  main: "Chicken Curry"
-};
+// const menu = {
+//   starter: "Chicken Wings",
+//   main: "Chicken Curry"
+// };
 
-const manager = "manager";
+// const manager = "manager";
 
-const hotelMenu = {
-  name: "ABC",
-  // ENHANCEMENT #1: instead of writing menu: menu, we can write only menu
-  menu,
+// const hotelMenu = {
+//   name: "ABC",
+//   // ENHANCEMENT #1: instead of writing menu: menu, we can write only menu
+//   menu,
 
-  // ENHANCEMENT #2: instead of writing methods as name: function(p1, p2){}, write as:
-  order(category){
-    if(category===0)
-      return menu.starter;
+//   // ENHANCEMENT #2: instead of writing methods as name: function(p1, p2){}, write as:
+//   order(category){
+//     if(category===0)
+//       return menu.starter;
 
-    return menu.main;
+//     return menu.main;
+//   },
+
+//   // ENHANCEMENT #3: We can evaluate property names in the object itself
+//   [`${manager}`+"Name"]: "Rishabh"
+
+// };
+
+// console.log(hotelMenu);
+
+
+
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
   },
-
-  // ENHANCEMENT #3: We can evaluate property names in the object itself
-  [`${manager}`+"Name"]: "Rishabh"
-
 };
 
-console.log(hotelMenu);
+// ------------------------------- CODING CHALLENGE #1 -----------------------------------
+/* 
+We're building a football betting app (soccer for my American friends 😅)!
+
+Suppose we get data from a web service about a certain game (below). In this challenge we're gonna work with the data. 
+So here are your tasks:
+
+1. Create one player array for each team (variables 'players1' and 'players2')
+2. The first player in any player array is the goalkeeper and the others are field players. For Bayern Munich (team 1) create 
+one variable ('gk') with the goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10 field players
+3. Create an array 'allPlayers' containing all players of both teams (22 players)
+4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a new array ('players1Final') containing all 
+the original team1 players plus 'Thiago', 'Coutinho' and 'Perisic'
+5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array) and prints each of them 
+to the console, along with the number of goals that were scored in total (number of player names passed in)
+7. The team with the lower odd is more likely to win. Print to the console which team is more likely to win, WITHOUT using 
+an if/else statement or the ternary operator.
+
+TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Then, call the function again with players from 
+game.scored
+
+GOOD LUCK 😀
+*/
+
+
+// 1
+const {players: [players1, players2] } = game;
+
+// 2
+const [gk, ...fieldPlayers] = players1;
+
+// 3
+const allPlayers = [...players1, ...players2];
+
+// 4
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+
+// 5
+const {odds: {team1, x:draw, team2}} = game;
+
+// 6
+const printGoals = function(...names){
+  for(const x of names){
+    console.log(x);
+  }
+  console.log(`Total Goals: ${names.length}`);
+};
+
+printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+printGoals(...game.scored);
+
+// 7
+team1 < team2 && console.log("Team 1 is more likely to win!");
+team2 < team1 && console.log("Team 2 is more likely to win!");
