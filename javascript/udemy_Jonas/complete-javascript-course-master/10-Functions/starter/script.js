0257fd55d01f277e02bd1b8492df03a37f5309ff
rishabhsxn <rishabhsaxena1998@gmail.com>
2021-01-27 20:21:02 +0530

    Higher-Order functions & Callback functions

--- javascript/udemy_Jonas/complete-javascript-course-master/10-Functions/starter/script.js
+++ javascript/udemy_Jonas/complete-javascript-course-master/10-Functions/starter/script.js
@@ -28,38 +28,62 @@
 
 
 
-// ---------------------------- PASSING ARGUMENTS: VALUE VS REFERENCE -----------------------------
+// // ---------------------------- PASSING ARGUMENTS: VALUE VS REFERENCE -----------------------------
 
-/* Every argument passing in JavaScript is pass by value. 
-There is no pass by reference in JavaScript. But, in case of objects, the variable itself is a reference, so the
-changes done inside the function also reflect on the original object. 
-It is called CALL-BY-SHARING */
+// /* IMPORTANT: Every argument passing in JavaScript is pass by value. 
+// There is no pass by reference in JavaScript. But, in case of objects, the variable itself is a reference, so the
+// changes done inside the function also reflect on the original object. 
+// It is called CALL-BY-SHARING */
 
-const flight = 'LH234';
+// const flight = 'LH234';
 
-const rishabh = {
-    name: 'Rishabh Saxena'
-};
+// const rishabh = {
+//     name: 'Rishabh Saxena'
+// };
 
-const rishabh2 = {
-    name: 'Rishabh Saxena'
-};
+// const rishabh2 = {
+//     name: 'Rishabh Saxena'
+// };
 
 
-const changeStuff = function(a, b, c){
-    a = a * 10;
+// const changeStuff = function(a, b, c){
+//     a = a * 10;
 
-    /* b variable is a value which is itself reference to rishabh object, so change will reflect in original object */
-    b.name = 'Mr. ' + b.name;       
+//     /* b variable is a value which is itself reference to rishabh object, so change will reflect in original object */
+//     b.name = 'Mr. ' + b.name;       
 
-    /* c, which initially holds reference to rishabh2, is made to hold another object's address. So, no change in rishabh2 */
-    c = {
-        name: 'Mr. Rishabh Saxena'
-    };
+//     /* c, which initially holds reference to rishabh2, is made to hold another object's address. So, no change in rishabh2 */
+//     c = {
+//         name: 'Mr. Rishabh Saxena'
+//     };
+// }
+
+// changeStuff(flight, rishabh, rishabh2);
+
+// console.log(flight);
+// console.log(rishabh);
+// console.log(rishabh2);
+
+
+
+// -------------------------------- HIGHER-ORDER FUNCTIONS ------------------------------------
+
+const oneWord = function(str){
+    return str.replaceAll(' ', '').toLowerCase();
 }
 
-changeStuff(flight, rishabh, rishabh2);
+const upperFirstWord = function(str){
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
+}
 
-console.log(flight);
-console.log(rishabh);
-console.log(rishabh2);
+// Higher order function - a function is passed as argument
+const transformer = function(str, fn){
+    console.log(`Original String: ${str}`);
+    console.log(`Transformed String: ${fn(str)}`);
+    console.log(`Transformed by function: ${fn.name}`);     // functions have property - name
+}
+
+// the passed functions are called Callbacks because these are called by the higher-order function anytime later
+transformer('JavaScript is the best language!', upperFirstWord);
+transformer('JavaScript is the best language!', oneWord);
