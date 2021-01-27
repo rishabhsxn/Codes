053c8d3a0e3bbbcf94b63c429b090cf046d283ba
rishabhsxn <rishabhsaxena1998@gmail.com>
2021-01-27 12:30:47 +0530

    Default Parameters - ES5 & ES6 way

--- javascript/udemy_Jonas/complete-javascript-course-master/10-Functions/starter/script.js
+++ javascript/udemy_Jonas/complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+
+// --------------------------- DEFAULT PARAMETERS ----------------------------------
+
+const bookings = [];
+
+// ES6 way - We can also use previously defined parameters in other parameters
+const createBooking = function(flightName, numPassengers = 1, price = 199 * numPassengers){
+
+    // // ES5 way of setting default parameters using Short Circuiting
+    // numPassengers = numPassengers || 1;
+    // price = price || 199;
+
+    const booking = {
+        flightName,
+        numPassengers,
+        price
+    };
+
+    console.log(booking);
+    bookings.push(booking);
+}
+
+createBooking('A320B');
+createBooking('B120Z', 2);
+createBooking('B120Z', undefined, 1000);    // use undefined if you want to skip a middle parameter (use it's default value)
