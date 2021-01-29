359793744f4ba87c74171229f97c28e8214a20e8
rishabhsxn <rishabhsaxena1998@gmail.com>
2021-01-29 13:40:42 +0530

    Immediately Invoked Function Expressions

--- javascript/udemy_Jonas/complete-javascript-course-master/10-Functions/starter/script.js
+++ javascript/udemy_Jonas/complete-javascript-course-master/10-Functions/starter/script.js
@@ -212,90 +212,115 @@
 
 
 
-// ----------------------------------- CODING CHALLENGE #1 ---------------------------------------------
-/* 
-Let's build a simple poll app!
+// // ----------------------------------- CODING CHALLENGE #1 ---------------------------------------------
+// /* 
+// Let's build a simple poll app!
 
-A poll has a question, an array of options from which people can choose, and an array with the number of replies for 
-each option. This data is stored in the starter object below.
+// A poll has a question, an array of options from which people can choose, and an array with the number of replies for 
+// each option. This data is stored in the starter object below.
 
-Here are your tasks:
+// Here are your tasks:
 
-1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
-  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
-        What is your favourite programming language?
-        0: JavaScript
-        1: Python
-        2: Rust
-        3: C++
-        (Write option number)
+// 1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+//   1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+//         What is your favourite programming language?
+//         0: JavaScript
+//         1: Python
+//         2: Rust
+//         3: C++
+//         (Write option number)
   
-  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT 
-  POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 
-  52 wouldn't make sense, right?)
+//   1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT 
+//   POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 
+//   52 wouldn't make sense, right?)
 
-2. Call this method whenever the user clicks the "Answer poll" button.
+// 2. Call this method whenever the user clicks the "Answer poll" button.
 
-3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), 
-which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). 
-This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+// 3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), 
+// which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). 
+// This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
 
-4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+// 4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
 
-HINT: Use many of the tools you learned about in this and the last section 😉
+// HINT: Use many of the tools you learned about in this and the last section 😉
 
-BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' 
-option. Do NOT put the arrays in the poll object! So what should the this keyword look like in this situation?
+// BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' 
+// option. Do NOT put the arrays in the poll object! So what should the this keyword look like in this situation?
 
-BONUS TEST DATA 1: [5, 2, 3]
-BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+// BONUS TEST DATA 1: [5, 2, 3]
+// BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
 
-GOOD LUCK 😀
-*/
+// GOOD LUCK 😀
+// */
 
-const poll = {
-    question: 'What is your favourite programming language?',
-    options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+// const poll = {
+//     question: 'What is your favourite programming language?',
+//     options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
 
-    // this generates [0, 0, 0, 0].
-    answers: new Array(4).fill(0),
-};
+//     // this generates [0, 0, 0, 0].
+//     answers: new Array(4).fill(0),
+// };
 
-// 3
-poll.displayResults = function(type='array'){
-    if(type === 'array')
-        console.log(this.answers);
-    else if(type === 'string')
-        console.log(`Poll results are ${this.answers.join(', ')}`)
-}
+// // 3
+// poll.displayResults = function(type='array'){
+//     if(type === 'array')
+//         console.log(this.answers);
+//     else if(type === 'string')
+//         console.log(`Poll results are ${this.answers.join(', ')}`)
+// }
 
-// 1
-poll.registerNewAnswer = function(){
-    let questionStr = `${this.question}\n${this.options.join('\n')}\n(Write option number)`;
-    const input = Number(prompt(questionStr));
+// // 1
+// poll.registerNewAnswer = function(){
+//     let questionStr = `${this.question}\n${this.options.join('\n')}\n(Write option number)`;
+//     const input = Number(prompt(questionStr));
     
-    console.log(input);
-    typeof input === 'number' && input > -1 && input < this.answers.length && this.answers[input]++;
+//     console.log(input);
+//     typeof input === 'number' && input > -1 && input < this.answers.length && this.answers[input]++;
 
-    // 4
-    this.displayResults();
-    this.displayResults('string');
+//     // 4
+//     this.displayResults();
+//     this.displayResults('string');
+// }
+
+// // 2
+// document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+
+// // bonus
+// const testData1 = {
+//     answers: [5, 2, 3],
+// };
+// const testData2 = {
+//     answers: [1, 5, 3, 9, 6, 1],
+// }
+
+// poll.displayResults.call(testData1);
+// poll.displayResults.call(testData1, 'string');
+
+// poll.displayResults.call(testData2);
+// poll.displayResults.call(testData2, 'string');
+
+
+
+// ---------------------------- IMMEDIATELY INVOKED FUNCTION EXPRESSIONS ------------------------------
+
+// It is a pattern not a feature of JS
+
+// we want a one-time use only function OR data hiding
+
+(function(){
+    console.log('This will run only once');
+    const privateVar = 10;      // data hiding
+})();
+
+// console.log(privateVar);    // not accessible outside
+
+// arrow function version
+(() => console.log('This will also only run once'))();
+
+
+// it is not anymore used for data hiding, because we can do it in more simple way 
+{
+    const privateNumber = 10;
 }
-
-// 2
-document.querySelector('.poll').addEventListener('click', poll.registerNewAnswer.bind(poll));
-
-
-// bonus
-const testData1 = {
-    answers: [5, 2, 3],
-};
-const testData2 = {
-    answers: [1, 5, 3, 9, 6, 1],
-}
-
-poll.displayResults.call(testData1);
-poll.displayResults.call(testData1, 'string');
-
-poll.displayResults.call(testData2);
-poll.displayResults.call(testData2, 'string');
+// console.log(privateNumber);     // not accessible
