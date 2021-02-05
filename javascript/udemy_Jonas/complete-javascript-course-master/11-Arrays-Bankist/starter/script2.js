e5fc2ef0b3e0c234f9771ad695776793e0b3ee72
rishabhsxn <rishabhsaxena1998@gmail.com>
2021-02-05 22:23:57 +0530

    Array Data Transformation: using map()

--- javascript/udemy_Jonas/complete-javascript-course-master/11-Arrays-Bankist/starter/script2.js
+++ javascript/udemy_Jonas/complete-javascript-course-master/11-Arrays-Bankist/starter/script2.js
@@ -1,50 +1,74 @@
 'use strict';
 
-// --------------------------------- CODING CHALLENGE #1 --------------------------------------
+// // --------------------------------- CODING CHALLENGE #1 --------------------------------------
 
-/* 
-Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored 
-the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an 
-adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+// /* 
+// Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored 
+// the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an 
+// adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
 
-Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the 
-following things:
+// Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the 
+// following things:
 
-1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a 
-shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to 
-mutate function parameters)
-2. Create an array with both Julia's (corrected) and Kate's data
-3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years 
-old") or a puppy ("Dog number 2 is still a puppy 🐶")
-4. Run the function for both test datasets
+// 1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a 
+// shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to 
+// mutate function parameters)
+// 2. Create an array with both Julia's (corrected) and Kate's data
+// 3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years 
+// old") or a puppy ("Dog number 2 is still a puppy 🐶")
+// 4. Run the function for both test datasets
 
-HINT: Use tools from all lectures in this section so far 😉
+// HINT: Use tools from all lectures in this section so far 😉
 
-TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
-TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+// TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+// TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
 
-GOOD LUCK 😀
-*/
+// GOOD LUCK 😀
+// */
 
-const checkDogs = function(dogsJulia, dogsKate){
-    // 1
-    const dogsJuliaCopy = dogsJulia.slice(1, -2);
-    console.log(...dogsJuliaCopy);
-    console.log(...dogsKate);
+// const checkDogs = function(dogsJulia, dogsKate){
+//     // 1
+//     const dogsJuliaCopy = dogsJulia.slice(1, -2);
+//     console.log(...dogsJuliaCopy);
+//     console.log(...dogsKate);
 
-    // 2
-    const allDogs = [...dogsJuliaCopy, ...dogsKate];
-    console.log(...allDogs);
+//     // 2
+//     const allDogs = [...dogsJuliaCopy, ...dogsKate];
+//     console.log(...allDogs);
 
-    // 3
-    allDogs.forEach(function(dogAge, i){
-        if(dogAge >= 3)
-            console.log(`Dog number ${i+1} is an adult, and is ${dogAge} years old`);
-        else
-            console.log(`Dog number ${i+1} is still a puppy 🐶`)
-    })
-};
+//     // 3
+//     allDogs.forEach(function(dogAge, i){
+//         if(dogAge >= 3)
+//             console.log(`Dog number ${i+1} is an adult, and is ${dogAge} years old`);
+//         else
+//             console.log(`Dog number ${i+1} is still a puppy 🐶`)
+//     })
+// };
 
-// 4
-checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
-checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+// // 4
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+// checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+
+
+
+
+// ---------------------------- ARRAY DATA TRANSFORMATION: MAPS -----------------------------------
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const euroToUsd = 1.1;
+
+// const movementsUSD = movements.map(function(mov){
+//     return mov * euroToUsd;
+// });
+
+const movementsUSD = movements.map(mov => mov * euroToUsd);
+
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsDescription = movements.map(
+    (mov, i) => `Movement #${i+1}: You ${mov > 0 ? 'Deposited' : 'Withdrew'} $${Math.abs(mov)}`
+);
+
+console.log(movementsDescription);
