2a9b825597db401befde078cf2a811acf6fe153d
rishabhsxn <rishabhsaxena1998@gmail.com>
2021-02-17 17:50:46 +0530

    sort() method

--- javascript/udemy_Jonas/complete-javascript-course-master/11-Arrays-Bankist/starter/script2.js
+++ javascript/udemy_Jonas/complete-javascript-course-master/11-Arrays-Bankist/starter/script2.js
@@ -177,51 +177,68 @@
 
 
 
-// ------------------------------------ FLAT() & FLATMAP() -----------------------------------
-const arr = [1, 2, 3, [4, 5, [6, 7], 8, [9, 10]]];
-console.log(arr.flat());
-console.log(arr.flat(2));
+// // ------------------------------------ FLAT() & FLATMAP() -----------------------------------
+// const arr = [1, 2, 3, [4, 5, [6, 7], 8, [9, 10]]];
+// console.log(arr.flat());
+// console.log(arr.flat(2));
 
-// Data
-const account1 = {
-    owner: 'Jonas Schmedtmann',
-    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-    interestRate: 1.2, // %
-    pin: 1111,
-};
+// // Data
+// const account1 = {
+//     owner: 'Jonas Schmedtmann',
+//     movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+//     interestRate: 1.2, // %
+//     pin: 1111,
+// };
   
-const account2 = {
-    owner: 'Jessica Davis',
-    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-    interestRate: 1.5,
-    pin: 2222,
-};
+// const account2 = {
+//     owner: 'Jessica Davis',
+//     movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+//     interestRate: 1.5,
+//     pin: 2222,
+// };
 
-const account3 = {
-    owner: 'Steven Thomas Williams',
-    movements: [200, -200, 340, -300, -20, 50, 400, -460],
-    interestRate: 0.7,
-    pin: 3333,
-};
+// const account3 = {
+//     owner: 'Steven Thomas Williams',
+//     movements: [200, -200, 340, -300, -20, 50, 400, -460],
+//     interestRate: 0.7,
+//     pin: 3333,
+// };
 
-const account4 = {
-    owner: 'Sarah Smith',
-    movements: [430, 1000, 700, 50, 90],
-    interestRate: 1,
-    pin: 4444,
-};
+// const account4 = {
+//     owner: 'Sarah Smith',
+//     movements: [430, 1000, 700, 50, 90],
+//     interestRate: 1,
+//     pin: 4444,
+// };
 
-const accounts = [account1, account2, account3, account4];
+// const accounts = [account1, account2, account3, account4];
 
-// get all movements in a single array
-const allAccountMovements = accounts
-    .map( acc => acc.movements )
-    .flat();
+// // get all movements in a single array
+// const allAccountMovements = accounts
+//     .map( acc => acc.movements )
+//     .flat();
 
-console.log(allAccountMovements);
+// console.log(allAccountMovements);
 
-// It turned out that flat is very often used after the map() method, so flatMap() was introduced
-const allAccountMovements2 = accounts
-    .flatMap( acc => acc.movements );
+// // It turned out that flat is very often used after the map() method, so flatMap() was introduced
+// const allAccountMovements2 = accounts
+//     .flatMap( acc => acc.movements );
 
-console.log(allAccountMovements2);
+// console.log(allAccountMovements2);
+
+
+
+// ------------------------------------------- SORT() ------------------------------------------
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// sort() mutates the original array
+
+/* IMPORTANT: By default, the sort method converts everything to string and then sort.
+So, it will not work correctlt with numbers.
+For Numbers, use a callback in sort */
+console.log(movements.sort());
+
+/* return > 0 -------> current, next  (keep order)
+   return < 0 -------> next, current  (switch order) */
+console.log(movements.sort( (current, next) => current - next));    // ascending
+console.log(movements.sort( (current, next) => next - current));    // descending
