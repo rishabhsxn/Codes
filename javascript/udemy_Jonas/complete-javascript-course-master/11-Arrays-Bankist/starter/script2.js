0313946e894abdd0474eb64afb34ee947f402b78
rishabhsxn <rishabhsaxena1998@gmail.com>
2021-02-17 17:19:05 +0530

    flat() & flatMap()

--- javascript/udemy_Jonas/complete-javascript-course-master/11-Arrays-Bankist/starter/script2.js
+++ javascript/udemy_Jonas/complete-javascript-course-master/11-Arrays-Bankist/starter/script2.js
@@ -164,13 +164,64 @@
 
 
 
-// --------------------------------- SOME() & EVERY() ---------------------------------
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-const movements2 = [200, 450, 3000, 70, 1300];
+// // --------------------------------- SOME() & EVERY() ---------------------------------
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements2 = [200, 450, 3000, 70, 1300];
 
-const depositsFun = mov => mov > 0;
+// const depositsFun = mov => mov > 0;
 
-console.log(movements.some(depositsFun));
+// console.log(movements.some(depositsFun));
 
-console.log(movements.every(depositsFun));
-console.log(movements2.every(depositsFun));
+// console.log(movements.every(depositsFun));
+// console.log(movements2.every(depositsFun));
+
+
+
+// ------------------------------------ FLAT() & FLATMAP() -----------------------------------
+const arr = [1, 2, 3, [4, 5, [6, 7], 8, [9, 10]]];
+console.log(arr.flat());
+console.log(arr.flat(2));
+
+// Data
+const account1 = {
+    owner: 'Jonas Schmedtmann',
+    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    interestRate: 1.2, // %
+    pin: 1111,
+};
+  
+const account2 = {
+    owner: 'Jessica Davis',
+    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    interestRate: 1.5,
+    pin: 2222,
+};
+
+const account3 = {
+    owner: 'Steven Thomas Williams',
+    movements: [200, -200, 340, -300, -20, 50, 400, -460],
+    interestRate: 0.7,
+    pin: 3333,
+};
+
+const account4 = {
+    owner: 'Sarah Smith',
+    movements: [430, 1000, 700, 50, 90],
+    interestRate: 1,
+    pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// get all movements in a single array
+const allAccountMovements = accounts
+    .map( acc => acc.movements )
+    .flat();
+
+console.log(allAccountMovements);
+
+// It turned out that flat is very often used after the map() method, so flatMap() was introduced
+const allAccountMovements2 = accounts
+    .flatMap( acc => acc.movements );
+
+console.log(allAccountMovements2);
