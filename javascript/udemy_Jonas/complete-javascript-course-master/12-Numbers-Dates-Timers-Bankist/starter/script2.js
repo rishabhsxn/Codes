6e22bd317ddd29b69e8a02968226a86508cda355
rishabhsxn <rishabhsaxena1998@gmail.com>
2021-02-27 21:18:14 +0530

    setTimeOut() & setInterval()

--- javascript/udemy_Jonas/complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script2.js
+++ javascript/udemy_Jonas/complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script2.js
@@ -34,40 +34,64 @@
 
 
 
-// ----------------------------------------- MATH -------------------------------------------------
+// // ----------------------------------------- MATH -------------------------------------------------
 
-console.log(Math.sqrt(25));
-// other way to find square root
-console.log(25 ** (1/2));
-console.log(8 ** (1/3));    // only way to find a cube root
+// console.log(Math.sqrt(25));
+// // other way to find square root
+// console.log(25 ** (1/2));
+// console.log(8 ** (1/3));    // only way to find a cube root
 
-// max min
-console.log(Math.max(12, 1, 45, 22));
-console.log(Math.max(12, 1, '45', 22));     // it performs type coersion but no parsing
-console.log(Math.max(12, 1, '45px', 22));
+// // max min
+// console.log(Math.max(12, 1, 45, 22));
+// console.log(Math.max(12, 1, '45', 22));     // it performs type coersion but no parsing
+// console.log(Math.max(12, 1, '45px', 22));
 
-console.log(Math.min(12, 1, '45', 22));
+// console.log(Math.min(12, 1, '45', 22));
 
-// constants
-console.log(Math.PI);
+// // constants
+// console.log(Math.PI);
 
-// boundaries are included
-const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-console.log(randomInt(10, 20));
+// // boundaries are included
+// const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+// console.log(randomInt(10, 20));
 
-// Rounding numbers - round, floor, ceil
-console.log(Math.round(23.3));
-console.log(Math.round(23.9));
+// // Rounding numbers - round, floor, ceil
+// console.log(Math.round(23.3));
+// console.log(Math.round(23.9));
 
-console.log(Math.ceil(23.3));
-console.log(Math.ceil(23.9));
+// console.log(Math.ceil(23.3));
+// console.log(Math.ceil(23.9));
 
-console.log(Math.floor(23.3));
-console.log(Math.floor(23.9));
+// console.log(Math.floor(23.3));
+// console.log(Math.floor(23.9));
 
-// IMPORTANT: floor() & trunc() work similarly for positive numbers but not for negative numbers
-console.log(Math.trunc(-23.3));
-console.log(Math.floor(-23.3));
+// // IMPORTANT: floor() & trunc() work similarly for positive numbers but not for negative numbers
+// console.log(Math.trunc(-23.3));
+// console.log(Math.floor(-23.3));
 
-// toFixed(): called upon a primitive (boxing), round it to specified points and return as STRING
-console.log(+(2.365).toFixed(1));
+// // toFixed(): called upon a primitive (boxing), round it to specified points and return as STRING
+// console.log(+(2.365).toFixed(1));
+
+
+
+// ---------------------------------- SETTIMEOUT() & SETINTERVAL() -------------------------------------------
+
+// setTimeOut
+const ingredients = ['olive', 'chicken'];
+
+/* We can assign the timer to a variable
+IMPORTANT: All the arguments after the time parameter, will be passed to the callback function */
+const pizzaTimer = setTimeout((ing1, ing2) => {
+    console.log(`Here is you Pizza with ${ing1} & ${ing2} 🍕`)
+    }, 3000, ...ingredients);
+
+// to prove that the code is Asynchronous
+console.log('Waiting...');
+
+// We can also cancel a callback from executing before it's execution
+if(ingredients.includes('spinach'))
+    clearTimeout(pizzaTimer);
+
+
+// setInterval - executes after every time interval that is mentioned
+setInterval(() => console.log(new Date()), 2000);
