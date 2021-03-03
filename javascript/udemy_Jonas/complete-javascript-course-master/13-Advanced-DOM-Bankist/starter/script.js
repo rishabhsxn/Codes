f3ff1e4879ba658a75747c49f0497038a6dd7bc0
rishabhsxn <rishabhsaxena1998@gmail.com>
2021-03-03 13:09:00 +0530

    Implement Smooth Scroll to 'Learn More' Button: New way

--- javascript/udemy_Jonas/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
+++ javascript/udemy_Jonas/complete-javascript-course-master/13-Advanced-DOM-Bankist/starter/script.js
@@ -35,20 +35,23 @@ document.addEventListener('keydown', function (e) {
 document.querySelector('.btn--scroll-to').addEventListener('click', function(event){
   const section1 = document.querySelector('#section--1');
 
-  // old way
-  // console.log('Button Position:', event.target.getBoundingClientRect())
-  const s1Coordinates = section1.getBoundingClientRect();
-  console.log('Current Section1 Position:', s1Coordinates);
-  console.log('Current Scrolled(x, y):', window.pageXOffset, window.pageYOffset);
-  console.log('Scrolled Distance(x, y):', s1Coordinates.x + window.pageXOffset, s1Coordinates.y + window.pageYOffset)
-  // window.scrollTo(s1Coordinates.x + window.pageXOffset, s1Coordinates.y + window.pageYOffset);
-  // for smooth scrolling:
-  window.scrollTo({
-    left: s1Coordinates.x + window.pageXOffset, 
-    top: s1Coordinates.y + window.pageYOffset,
-    behavior: 'smooth'
-  });
+  // // old way
+  // // console.log('Button Position:', event.target.getBoundingClientRect())
+  // const s1Coordinates = section1.getBoundingClientRect();
+  // console.log('Current Section1 Position:', s1Coordinates);
+  // console.log('Current Scrolled(x, y):', window.pageXOffset, window.pageYOffset);
+  // console.log('Scrolled Distance(x, y):', s1Coordinates.x + window.pageXOffset, s1Coordinates.y + window.pageYOffset)
+  // // window.scrollTo(s1Coordinates.x + window.pageXOffset, s1Coordinates.y + window.pageYOffset);
+  // // for smooth scrolling:
+  // window.scrollTo({
+  //   left: s1Coordinates.x + window.pageXOffset, 
+  //   top: s1Coordinates.y + window.pageYOffset,
+  //   behavior: 'smooth'
+  // });
 
-  // // for getting current height and width'
+  // for getting current height and width'
   // console.log('Height, Width:', document.documentElement.clientHeight, document.documentElement.clientWidth);
+
+  // new way
+  section1.scrollIntoView({ behavior: 'smooth' });
 });
