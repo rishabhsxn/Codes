96362aea7e47173f7fa22d46fa9d10809e16ec59
rishabhsxn <rishabhsaxena1998@gmail.com>
2021-05-08 17:22:25 +0530

    Convert useState() to useReducer()

--- Mobile_Development/React_Native/udemy_01/rn-starter-main/src/screens/SquareScreen.js
+++ Mobile_Development/React_Native/udemy_01/rn-starter-main/src/screens/SquareScreen.js
@@ -1,40 +1,67 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import ColorAdjuster from "../components/ColorAdjuster";
 
 const COLOR_CHANGE_FACTOR = 15;
 
-const SquareScreen = () => {
-	const [redCount, setRedCount] = useState(0);
-	const [greenCount, setGreenCount] = useState(0);
-	const [blueCount, setBlueCount] = useState(0);
+/* Reducer   ===  functionToManageChangesToAnObject
+   Dispatch  ===  runMyReducer
+   Action    ===  howToChangeState 
+*/
 
+const reducer = (state, action) => {
+	// state === { red: number, green: number, blue: number }
+	// action === { whatToDo: 'change_red' || 'change_green' || 'change_blue', amount: number }
+
+	switch (action.type) {
+		/* Don't mutate the state directly, instead pass a new state object.
+		{ ...state, red: state.red } will spread the previous state into new object and overwrite the property red */
+		case "CHANGE_RED":
+			return { ...state, red: state.red + action.payload };
+
+		case "CHANGE_GREEN":
+			return { ...state, green: state.green + action.payload };
+
+		case "CHANGE_BLUE":
+			return { ...state, blue: state.blue + action.payload };
+
+		default:
+			return state;
+	}
+};
+
+const SquareScreen = () => {
+	const [state, dispatch] = useReducer(reducer, {
+		red: 0,
+		green: 0,
+		blue: 0,
+	});
+
+	const { red, green, blue } = state;
+
+	// prettier-ignore
 	return (
 		<View style={{ marginHorizontal: 15 }}>
 			<ColorAdjuster
 				colorName="Red"
-				onIncrease={() => setRedCount(redCount + COLOR_CHANGE_FACTOR)}
-				onDecrease={() => setRedCount(redCount - COLOR_CHANGE_FACTOR)}
+				onIncrease={() => dispatch({ type: "CHANGE_RED", payload: COLOR_CHANGE_FACTOR })}
+				onDecrease={() => dispatch({ type: "CHANGE_RED", payload: -1 * COLOR_CHANGE_FACTOR })}
 			/>
 			<ColorAdjuster
 				colorName="Green"
-				onIncrease={() =>
-					setGreenCount(greenCount + COLOR_CHANGE_FACTOR)
-				}
-				onDecrease={() =>
-					setGreenCount(greenCount - COLOR_CHANGE_FACTOR)
-				}
+				onIncrease={() => dispatch({ type: "CHANGE_GREEN", payload: COLOR_CHANGE_FACTOR })}
+				onDecrease={() => dispatch({ type: "CHANGE_GREEN", payload: -1 * COLOR_CHANGE_FACTOR })}
 			/>
 			<ColorAdjuster
 				colorName="Blue"
-				onIncrease={() => setBlueCount(blueCount + COLOR_CHANGE_FACTOR)}
-				onDecrease={() => setBlueCount(blueCount - COLOR_CHANGE_FACTOR)}
+				onIncrease={() => dispatch({ type: "CHANGE_BLUE", payload: COLOR_CHANGE_FACTOR })}
+				onDecrease={() => dispatch({ type: "CHANGE_BLUE", payload: -1 * COLOR_CHANGE_FACTOR })}
 			/>
 			<View
 				style={{
 					height: 100,
 					width: 100,
-					backgroundColor: `rgb(${redCount}, ${greenCount}, ${blueCount})`,
+					backgroundColor: `rgb(${red}, ${green}, ${blue})`,
 					marginTop: 15,
 				}}
 			></View>
