8df80793cc4cdd9caa0d0d0f8f20651ee0368b62
rishabhsxn <rishabhsaxena1998@gmail.com>
2021-05-08 21:03:10 +0530

    Convert useState() to useReducer()

--- Mobile_Development/React_Native/udemy_01/rn-starter-main/src/screens/CounterScreen.js
+++ Mobile_Development/React_Native/udemy_01/rn-starter-main/src/screens/CounterScreen.js
@@ -1,8 +1,22 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 
+const INCREMENT_FACTOR = 1;
+
+const reducer = (state, action) => {
+	switch (action.type) {
+		case "INCREASE_COUNTER":
+			return { ...state, counter: state.counter + action.payload };
+		case "DECREASE_COUNTER":
+			return { ...state, counter: state.counter - action.payload };
+		default:
+			return state;
+	}
+};
+
 const CounterScreen = () => {
-	const [counter, setCounter] = useState(0); // [variableName, setterFunctionName] = useState(initialValue)
+	const [state, dispatch] = useReducer(reducer, { counter: 0 });
+	// const [counter, setCounter] = useState(0); // [variableName, setterFunctionName] = useState(initialValue)
 	// IMPORTANT: Don't modify the state varible directly, only use setterFunction
 
 	return (
@@ -10,18 +24,24 @@ const CounterScreen = () => {
 			<Button
 				title="Increase"
 				onPress={() => {
-					setCounter(counter + 1);
+					dispatch({
+						type: "INCREASE_COUNTER",
+						payload: INCREMENT_FACTOR,
+					});
 				}}
 			/>
 
 			<Button
 				title="Decrease"
 				onPress={() => {
-					setCounter(counter - 1);
+					dispatch({
+						type: "DECREASE_COUNTER",
+						payload: INCREMENT_FACTOR,
+					});
 				}}
 			/>
 
-			<Text>Counter: {counter}</Text>
+			<Text>Counter: {state.counter}</Text>
 		</View>
 	);
 };
