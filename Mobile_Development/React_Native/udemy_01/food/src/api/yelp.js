fce612a1168a3493b5056a1d2fad5b2d948e25eb
rishabhsxn <rishabhsaxena1998@gmail.com>
2021-05-10 01:01:57 +0530

    Change API key

--- Mobile_Development/React_Native/udemy_01/food/src/api/yelp.js
+++ Mobile_Development/React_Native/udemy_01/food/src/api/yelp.js
@@ -4,6 +4,6 @@ export default axios.create({
 	baseURL: "https://api.yelp.com/v3/businesses",
 	headers: {
 		Authorization:
-			"Bearer MMMQuWnLGwlTYdSMtKujs774rvSF8-g78jwHgo35nIoahZ1df13ph_HxFOTGIpGD-_CVpb--RMaPA2clqd8koS8x58EPk6H9fkhW791Uws_LRizh44UyVyGh4feNX3Yx",
+			"Bearer BLwsOxS9h7Nn2xiu-ts0ZIUxh5xRK98Rl9DdMqxaZhSg58IZ1ohuBtWJeV5sQtKZUL3sXF4xTca6NnXr5ws_Uq4qtq0x7kErSfYAPUZlXkVryiNav_lGN_LFBjeYYHYx",
 	},
 });
