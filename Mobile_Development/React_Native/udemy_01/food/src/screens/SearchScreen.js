46baeec4f4c4061ed9e556cda712430541d91c58
rishabhsxn <rishabhsaxena1998@gmail.com>
2021-05-09 21:13:07 +0530

    Initial Commit

---
+++ Mobile_Development/React_Native/udemy_01/food/src/screens/SearchScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const SearchScreen = () => {
+	return (
+		<View>
+			<Text>Search Screen</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
