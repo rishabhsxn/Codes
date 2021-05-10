b03fd8484899ae8d53b62864af7f4ec3d7ac392e
rishabhsxn <rishabhsaxena1998@gmail.com>
2021-05-10 14:44:41 +0530

    Separate hooks logic for yelp api into separate hook file

---
+++ Mobile_Development/React_Native/udemy_01/food/src/hooks/useResults.js
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+import yelp from "../api/yelp";
+
+export default () => {
+	const [results, setResults] = useState([]);
+	const [errorMessage, setErrorMessage] = useState("");
+
+	const searchAPI = async (searchTerm) => {
+		try {
+			const response = await yelp.get("/search", {
+				// These params will be added in the link as arguments
+				params: {
+					term: searchTerm,
+					limit: 50,
+					location: "san jose",
+				},
+			});
+
+			setResults(response.data.businesses);
+		} catch (err) {
+			setErrorMessage("Something went wrong.");
+		}
+	};
+
+	// this will run the callback only after the 1st render
+	useEffect(() => {
+		searchAPI("Pasta");
+	}, []);
+
+	return [searchAPI, results, errorMessage];
+};
+
+/* Steps to separate hooks logic from component:
+
+1. Cut all the code except the JSX that is related to business logic (in this case yelp api) and 
+move to a separate file.
+
+2. Find the variables and functions that are now needed in the JSX and return them in an array from the 
+separated hooks code.  */
--- Mobile_Development/React_Native/udemy_01/food/src/screens/SearchScreen.js
+++ Mobile_Development/React_Native/udemy_01/food/src/screens/SearchScreen.js
@@ -1,40 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
-import yelp from "../api/yelp";
+import useResults from "../hooks/useResults";
 
 const SearchScreen = () => {
 	const [term, setTerm] = useState("");
-	const [results, setResults] = useState([]);
-	const [errorMessage, setErrorMessage] = useState("");
-
-	const searchAPI = async (searchTerm) => {
-		try {
-			const response = await yelp.get("/search", {
-				// These params will be added in the link as arguments
-				params: {
-					term: searchTerm,
-					limit: 50,
-					location: "san jose",
-				},
-			});
-
-			setResults(response.data.businesses);
-		} catch (err) {
-			setErrorMessage("Something went wrong.");
-		}
-	};
-
-	useEffect(() => {
-		searchAPI("Pasta");
-	}, []); // this will run the callback only after the 1st render
+	const [searchAPI, results, errorMessage] = useResults();
 
 	return (
 		<View style={{ backgroundColor: "#fff", flex: 1 }}>
 			<SearchBar
 				term={term}
 				onTermChange={setTerm} // condensed form of {(newTerm) => setTerm(newTerm)}
-				onTermSubmit={searchAPI}
+				onTermSubmit={() => searchAPI(term)}
 			/>
 			{errorMessage ? <Text>{errorMessage}</Text> : null}
 			<Text>We have found {results.length} results.</Text>
