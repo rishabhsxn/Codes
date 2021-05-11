96053b7cb47c6e7d601ec7d263509b647c7b9a97
rishabhsxn <rishabhsaxena1998@gmail.com>
2021-05-11 16:33:04 +0530

    Setup navigator with initial IndexScreen

--- Mobile_Development/React_Native/udemy_01/blog/App.js
+++ Mobile_Development/React_Native/udemy_01/blog/App.js
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import IndexScreen from "./src/screens/IndexScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const navigator = createStackNavigator(
+	{
+		Index: IndexScreen,
+	},
+	{
+		initialRouteName: "Index",
+		defaultNavigationOptions: {
+			title: "Blogs",
+		},
+	}
+);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default createAppContainer(navigator);
---
+++ Mobile_Development/React_Native/udemy_01/blog/src/screens/IndexScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+const IndexScreen = () => {
+	return (
+		<View>
+			<Text>Index Screen</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
