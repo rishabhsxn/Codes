312b430cdb75b226e0b853ce8d4373f475232ffb
rishabhsxn <rishabhsaxena1998@gmail.com>
2021-05-12 02:35:17 +0530

    Style ShowScreen

--- Mobile_Development/React_Native/udemy_01/blog/src/screens/ShowScreen.js
+++ Mobile_Development/React_Native/udemy_01/blog/src/screens/ShowScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { BlogContext } from "../context/BlogContext";
+import { EvilIcons } from "@expo/vector-icons";
 
 const ShowScreen = ({ navigation }) => {
 	const { state } = useContext(BlogContext);
@@ -9,13 +10,37 @@ const ShowScreen = ({ navigation }) => {
 	const blogPost = state.find((blog) => blog.id === id);
 
 	return (
-		<View>
-			<Text>Show Screen</Text>
-			<Text>{blogPost.title}</Text>
+		<View style={styles.container}>
+			<Text style={styles.title}>{blogPost.title}</Text>
+			<Text style={styles.content}>{blogPost.content}</Text>
 		</View>
 	);
 };
 
-const styles = StyleSheet.create({});
+ShowScreen.navigationOptions = () => {
+	return {
+		headerRight: () => (
+			<TouchableOpacity>
+				<EvilIcons name="pencil" size={43} />
+			</TouchableOpacity>
+		),
+	};
+};
+
+const styles = StyleSheet.create({
+	container: {
+		padding: 20,
+		backgroundColor: "#fff",
+		flex: 1,
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: "bold",
+		marginBottom: 20,
+	},
+	content: {
+		fontSize: 18,
+	},
+});
 
 export default ShowScreen;
