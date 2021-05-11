12fdf847ad08bd5d63990c888199dbaa0db51da2
rishabhsxn <rishabhsaxena1998@gmail.com>
2021-05-12 00:46:03 +0530

    Add Delete icon to all Blogs

--- Mobile_Development/React_Native/udemy_01/blog/src/screens/IndexScreen.js
+++ Mobile_Development/React_Native/udemy_01/blog/src/screens/IndexScreen.js
@@ -1,24 +1,51 @@
 import React, { useContext } from "react";
-import { Text, View, StyleSheet, FlatList, Button } from "react-native";
+import {
+	Text,
+	View,
+	StyleSheet,
+	FlatList,
+	Button,
+	TouchableOpacity,
+} from "react-native";
 import { BlogContext } from "../context/BlogContext";
+import { Feather } from "@expo/vector-icons";
 
 const IndexScreen = () => {
 	const { state: blogs, addBlogPost } = useContext(BlogContext);
 
 	return (
-		<View>
+		<>
 			<Button title="Add Blog" onPress={addBlogPost} />
 			<FlatList
 				data={blogs}
 				keyExtractor={(blog) => blog.title}
 				renderItem={({ item: blog }) => {
-					return <Text>{blog.title}</Text>;
+					return (
+						<View style={styles.blog}>
+							<Text style={styles.blogTitle}>{blog.title}</Text>
+							<TouchableOpacity onPress={() => {}}>
+								<Feather size={30} name="trash" />
+							</TouchableOpacity>
+						</View>
+					);
 				}}
 			/>
-		</View>
+		</>
 	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	blog: {
+		flex: 1,
+		flexDirection: "row",
+		justifyContent: "space-between",
+		padding: 10,
+		borderColor: "#000",
+		borderBottomWidth: 1,
+	},
+	blogTitle: {
+		fontSize: 18,
+	},
+});
 
 export default IndexScreen;
