eb4a5fa54be464f9ee9da816861ef59337c5ad32
rishabhsxn <rishabhsaxena1998@gmail.com>
2021-05-14 16:25:33 +0530

    Setup mongodb connection using mongoose

--- Mobile_Development/React_Native/udemy_01/tracker-server/src/index.js
+++ Mobile_Development/React_Native/udemy_01/tracker-server/src/index.js
@@ -1,6 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const mongoUri =
+	"mongodb+srv://admin:passwordvar@cluster0.2tte6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+// connect to mongodb
+mongoose.connect(mongoUri, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true,
+});
+// connection listeners
+mongoose.connection.on("connected", () => {
+	console.log("Connected to mongo instance");
+});
+mongoose.connection.on("error", (err) => {
+	console.log("Error connecting to mongo:", err);
+});
 
 // first route
 app.get("/", (req, res) => {
