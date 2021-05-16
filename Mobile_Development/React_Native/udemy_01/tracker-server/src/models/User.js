22c2910d651d70880ddc2b19e8a3b01b4ee38304
rishabhsxn <rishabhsaxena1998@gmail.com>
2021-05-16 18:50:45 +0530

    Implement signin route with Hashed and Salted password

--- Mobile_Development/React_Native/udemy_01/tracker-server/src/models/User.js
+++ Mobile_Development/React_Native/udemy_01/tracker-server/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 // create a schema
 const userSchema = new mongoose.Schema({
@@ -12,6 +13,46 @@ const userSchema = new mongoose.Schema({
 		required: true,
 	},
 });
+
+// create function that will automatically run when we save user and convert password string using salting and hashing
+// pre-save hook
+userSchema.pre("save", function (next) {
+	const user = this;
+
+	// if the password is not changed, then don't do anything
+	if (!user.isModified("password")) {
+		return next();
+	}
+
+	// if password is newly added or updated, then convert
+	bcrypt.genSalt(10, (err, salt) => {
+		if (err) return next(err);
+
+		bcrypt.hash(user.password, salt, (err, hash) => {
+			if (err) return next(err);
+
+			// replace user password with hashed value
+			user.password = hash;
+			next();
+		});
+	});
+});
+
+// create a method to compare if user entered the correct password during signin
+userSchema.methods.comparePassword = function (enteredPassword) {
+	const user = this;
+
+	// creating a promise so that we can use the async await syntax
+	return new Promise((resolve, reject) => {
+		bcrypt.compare(enteredPassword, user.password, (err, isMatch) => {
+			if (err) reject(err);
+
+			if (!isMatch) reject(false);
+
+			resolve(true);
+		});
+	});
+};
 
 // create a model using the schema
 mongoose.model("User", userSchema);
--- Mobile_Development/React_Native/udemy_01/tracker-server/src/routes/authRoutes.js
+++ Mobile_Development/React_Native/udemy_01/tracker-server/src/routes/authRoutes.js
@@ -35,4 +35,24 @@ router.post("/signup", async (req, res) => {
 	}
 });
 
+router.post("/signin", async (req, res) => {
+	const { email, password } = req.body;
+
+	if (!email || !password)
+		return res.status(422).send({ error: "Must provide email & password" });
+
+	const user = await User.findOne({ email });
+	if (!user) return res.status(404).send({ error: "User not found" });
+
+	// if password doesn't match, then the promise will be rejected
+	try {
+		await user.comparePassword(password);
+		// when passwords are matched, send the jwt so that the client can save it
+		const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");
+		res.send({ token });
+	} catch (err) {
+		return res.status(422).send({ error: "Invalid email or password" });
+	}
+});
+
 module.exports = router;
