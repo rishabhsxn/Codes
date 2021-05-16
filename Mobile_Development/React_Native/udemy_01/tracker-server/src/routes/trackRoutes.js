63f44237a1580df88ed04078746ee6f48a5cff7c
rishabhsxn <rishabhsaxena1998@gmail.com>
2021-05-16 19:35:28 +0530

    Implement POST request on /tracks route

--- Mobile_Development/React_Native/udemy_01/tracker-server/src/routes/trackRoutes.js
+++ Mobile_Development/React_Native/udemy_01/tracker-server/src/routes/trackRoutes.js
@@ -16,4 +16,26 @@ router.get("/tracks", async (req, res) => {
 	res.send(tracks);
 });
 
+// route to add new tracks
+router.post("/tracks", async (req, res) => {
+	const { name, locations } = req.body;
+
+	if (!name || !locations)
+		return res
+			.status(422)
+			.send({ error: "You must provide a name and locations" });
+
+	// Saving the track may cause error if user has provided data which doesn't match the schema
+	try {
+		// create an instance of Track
+		const track = new Track({ name, locations, userId: req.user._id });
+		// save the track
+		await track.save();
+		// send the created track in response for now
+		res.send(track);
+	} catch (err) {
+		res.status(422).send({ error: err.message });
+	}
+});
+
 module.exports = router;
