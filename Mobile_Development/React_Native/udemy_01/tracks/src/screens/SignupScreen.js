80164d20fb0bca05004f5f49a5d77d9dea2d8bbd
rishabhsxn <rishabhsaxena1998@gmail.com>
2021-05-18 09:34:47 +0530

    Style Signup Screen

--- Mobile_Development/React_Native/udemy_01/tracks/src/screens/SignupScreen.js
+++ Mobile_Development/React_Native/udemy_01/tracks/src/screens/SignupScreen.js
@@ -1,26 +1,53 @@
-import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import { Text, Button, Input } from "react-native-elements";
+import Spacer from "../components/Spacer";
 
 const SignupScreen = ({ navigation }) => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
 	return (
-		<View>
-			<Text>SignupScreen</Text>
-			<Button
-				title="Go to Sign in"
-				onPress={() => {
-					navigation.navigate("Signin");
-				}}
-			/>
-			<Button
-				title="Go to mainflow"
-				onPress={() => {
-					navigation.navigate("mainFlow");
-				}}
-			/>
+		<View style={styles.container}>
+			<Spacer>
+				<Text h3>Sign Up for Tracks</Text>
+			</Spacer>
+			<Spacer>
+				<Input
+					label="Email"
+					value={email}
+					onChangeText={setEmail}
+					autoCapitalize="none"
+					autoCorrect={false}
+				/>
+				<Input
+					label="Password"
+					value={password}
+					onChangeText={setPassword}
+					autoCapitalize="none"
+					autoCorrect={false}
+					secureTextEntry
+				/>
+			</Spacer>
+			<Spacer>
+				<Button title="Sign Up" />
+			</Spacer>
 		</View>
 	);
 };
 
+SignupScreen.navigationOptions = () => {
+	return {
+		headerShown: false,
+	};
+};
+
 export default SignupScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: "center",
+		marginBottom: 200,
+	},
+});
