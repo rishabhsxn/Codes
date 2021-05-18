a7e971b2599979b97ca700ab8f2807026c73fd31
rishabhsxn <rishabhsaxena1998@gmail.com>
2021-05-18 08:57:20 +0530

    Hookup Navigation

--- Mobile_Development/React_Native/udemy_01/tracks/App.js
+++ Mobile_Development/React_Native/udemy_01/tracks/App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import { createBottomTabNavigator } from "react-navigation-tabs";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import SignupScreen from "./src/screens/SignupScreen";
+import SigninScreen from "./src/screens/SigninScreen";
+import TrackCreateScreen from "./src/screens/TrackCreateScreen";
+import TrackDetailsScreen from "./src/screens/TrackDetailsScreen";
+import TrackListScreen from "./src/screens/TrackListScreen";
+import AccountScreen from "./src/screens/AccountScreen";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const navigator = createSwitchNavigator({
+	loginFlow: createStackNavigator({
+		Signup: SignupScreen,
+		Signin: SigninScreen,
+	}),
+	mainFlow: createBottomTabNavigator({
+		trackListFlow: createStackNavigator({
+			TrackList: TrackListScreen,
+			TrackDetail: TrackDetailsScreen,
+		}),
+		TrackCreate: TrackCreateScreen,
+		Account: AccountScreen,
+	}),
 });
+
+export default createAppContainer(navigator);
