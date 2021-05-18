2c6672a90033af468fc27243a60482256f02460e
rishabhsxn <rishabhsaxena1998@gmail.com>
2021-05-18 10:09:50 +0530

    Avoid Screen moving up when using keyboard

--- Mobile_Development/React_Native/udemy_01/tracks/src/screens/SignupScreen.js
+++ Mobile_Development/React_Native/udemy_01/tracks/src/screens/SignupScreen.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-import { StyleSheet, View } from "react-native";
+import {
+	StyleSheet,
+	View,
+	KeyboardAvoidingView,
+	ScrollView,
+} from "react-native";
 import { Text, Button, Input } from "react-native-elements";
 import Spacer from "../components/Spacer";
 
@@ -8,31 +13,35 @@ const SignupScreen = ({ navigation }) => {
 	const [password, setPassword] = useState("");
 
 	return (
-		<View style={styles.container}>
-			<Spacer>
-				<Text h3>Sign Up for Tracks</Text>
-			</Spacer>
-			<Spacer>
-				<Input
-					label="Email"
-					value={email}
-					onChangeText={setEmail}
-					autoCapitalize="none"
-					autoCorrect={false}
-				/>
-				<Input
-					label="Password"
-					value={password}
-					onChangeText={setPassword}
-					autoCapitalize="none"
-					autoCorrect={false}
-					secureTextEntry
-				/>
-			</Spacer>
-			<Spacer>
-				<Button title="Sign Up" />
-			</Spacer>
-		</View>
+		<KeyboardAvoidingView>
+			<ScrollView contentContainerStyle={styles.container}>
+				<View>
+					<Spacer>
+						<Text h3>Sign Up for Tracks</Text>
+					</Spacer>
+					<Spacer>
+						<Input
+							label="Email"
+							value={email}
+							onChangeText={setEmail}
+							autoCapitalize="none"
+							autoCorrect={false}
+						/>
+						<Input
+							label="Password"
+							value={password}
+							onChangeText={setPassword}
+							autoCapitalize="none"
+							autoCorrect={false}
+							secureTextEntry
+						/>
+					</Spacer>
+					<Spacer>
+						<Button title="Sign Up" />
+					</Spacer>
+				</View>
+			</ScrollView>
+		</KeyboardAvoidingView>
 	);
 };
 
@@ -46,8 +55,6 @@ export default SignupScreen;
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
-		justifyContent: "center",
-		marginBottom: 200,
+		paddingVertical: 100,
 	},
 });
